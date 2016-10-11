a6e081d4dcec2fd279aabd0cba6cc7107b26f2f8
ruslan <ruslan@developmentontheedge.com>
2016-10-11 16:31:48 +0700

    Build fixes: 1. Move gretty-spock to test context 2. Use compability version of spockframework to gretty-spock

--- integrationTest.groovy
+++ integrationTest.groovy
@@ -16,7 +16,8 @@ apply plugin: 'groovy'
 
 dependencies {
     testCompile "org.codehaus.groovy:groovy-all:2.4.5"
-    testCompile group: 'org.spockframework', name: 'spock-core', version: '1.1-groovy-2.4-rc-2'
+    testCompile group: 'org.spockframework', name: 'spock-core', version: '1.0-groovy-2.4'
+    testCompile group: 'org.akhikhl.gretty', name: 'gretty-spock', version: '1.4.0'
 }
 
 test {
